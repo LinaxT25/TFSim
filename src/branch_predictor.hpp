77d73650d8495befe57e8ad962fd1ca5b9f1a24f
Vinícius Galvão <LinaxT25@users.noreply.github.com>
2019-11-28 23:45:45 -0400

    Update branch_predictor.hpp

--- src/branch_predictor.hpp
+++ src/branch_predictor.hpp
@@ -8,3 +8,15 @@ public:
 private:
     int n_bits,max,state;
 };
+
+/* Classe de herança da branch_predictor para implementação do BPB */
+
+class BPB : public branch_predictor
+{
+    public:
+        BPB();
+        BPB(unsigned int x) : num_buffers(x) {};
+        branch_predictor *Array_BPB = new branch_predictor[num_buffers];    
+    public:
+        unsigned int num_buffers, count = 0;      
+};
